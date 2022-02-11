b202950f0cb8b8ab81a21108c047eba372fc129b
Nan R <nanbusiness13@gmail.com>
2022-02-11 11:28:44 +0100

    Implement mongodb and cleanup

--- master/main.py
+++ master/main.py
@@ -1,6 +1,8 @@
 import random
+import pprint
 import interactions
-import json
+import pymongo
+from pymongo import MongoClient
 
 # Wordle!
 SCOPES = '749015533310967828'
@@ -16,6 +18,10 @@ wordle = WORDS[random.randint(0, len(WORDS))]
 player_current: list = []
 player_guesses: list = []
 today = '1'
+cluster = MongoClient('mongodb+srv://NeverOne:A6XwrU17hfsHKHbR@cluster0.ivnqs.mongodb.net/test')
+db = cluster['users_total']
+collection = db['users_total']
+posts = db.posts
 
 
 def gss(guess):
@@ -98,10 +104,8 @@ async def on_ready():
 
 @bot.command(name='dump', description='dumps', scope=SCOPES, options=[interactions.Option(
     name='user', description='User you want to dump', type=interactions.OptionType.STRING, required=True)])
-async def dump(ctx: interactions.CommandContext, user):
-    with open('../resources/users.json', 'r') as d:
-        data = json.load(d)
-    await ctx.send(str(dict(data[user])))
+async def dump(ctx: interactions.CommandContext):
+    await ctx.send('...')
 
 
 @bot.command(name='test', description='Sends a test command', scope=SCOPES)
@@ -114,42 +118,54 @@ async def emb(ctx: interactions.CommandContext):
 @bot.command(name='start', description='Creates a user profile so your progress and stats can be saved. It is '
                                        'required to play the game!', scope=SCOPES)
 async def start(ctx: interactions.CommandContext):
-    with open('../resources/users.json', 'r+') as m:
-        data = json.load(m)
-        if str(ctx.author.user.id) in data:
-            return await ctx.send('User profile already exists!')
-    data.update(profiler(ctx.author.user.id))
-    with open('../resources/users.json', 'w') as fm:
-        json.dump(data, fm)
-    await ctx.send('w')
+    if collection.find_one({"_id": int(ctx.author.user.id)}) is not None:
+        post = {"_id": int(ctx.author.user.id), "streak": 0, "mode": "Wordle", "beaten": False, "tries": [],
+                "guesses": []}
+        collection.insert_one(post)
+        await ctx.send(embeds=interactions.Embed(title='*Wordcord!*',
+                                                 description='Profile created in database',
+                                                 color=0x56AB91))
+    else:
+        return await ctx.send(embeds=interactions.Embed(title='*Wordcord!*',
+                                                        description='Profile already exists in database',
+                                                        color=0x56AB91))
 
 
 @bot.command(name='submit', description='Submit a wordle guess', scope=SCOPES, options=[interactions.Option(
     name='guess', description='A string containing your guess', type=interactions.OptionType.STRING, required=True)])
 async def submit(ctx: interactions.CommandContext, guess):
-    with open('../resources/users.json') as k:
-        if ctx.author.user.id not in k:
-            await ctx.send('Run /start ')
     if not err(guess):
-        return await ctx.send('Invalid input. Guesses must be 5 letter long words containing characters only from a '
-                              'to z.')
-    if len(player_current) != 0:
-        if wnc(player_current[-1]):
-            return await ctx.send('You already won, no need to try again.', ephemeral=True)
+        return await ctx.send('Invalid input. Guesses must be 5 letter long words containing characters only from a to z.')
     if guess not in WORDS:
         return await ctx.send('Word not in word list.')
-    if len(player_current) >= 6:
-        return await ctx.send(f'Max. number of tries reached. The word was ||{wordle}||', ephemeral=True)
-    player_current.append(gss(guess))
-    player_guesses.append(guess)
-    plygs = []
-    it = 0
-    for el in player_guesses:
-        plygs.append(f'{player_current[it]}  -->  {el}')
-        it += 1
+    if collection.find_one({"_id": int(ctx.author.user.id)}) is None:
+        return await ctx.send('Run /start so you can play!')
+    user = collection.find_one({"_id": int(ctx.author.user.id)})
+    if len(list(user["guesses"])) >= 6:
+        return await ctx.send('You already tried 6 times today!')
+    if bool(user["beaten"]):
+        return await ctx.send('You already beat the game today!')
     if wnc(gss(guess)):
-        return await ctx.send(f"Wordle #{today} - {len(player_current)}/6\n\n" + '\n'.join(player_current))
-    return await ctx.send(f"Wordle #{today} - {len(player_current)}/6\n\n" + '\n'.join(plygs), ephemeral=True)
+        return collection.update_one({"_id": ctx.author.user.id},
+                                     {"$set": {"guesses": list(user["guesses"]).append(guess),
+                                               "tries": list(user["tries"]).append(gss(guess)),
+                                               "beaten": True}})
+
+    collection.update_one({"_id": ctx.author.user.id},
+                          {"$set": {"guesses": list(user["guesses"]).append(guess),
+                                    "tries": list(user["tries"]).append(gss(guess))}})
 
 
 bot.start()
+
+"""
+    if len(player_current) != 0:
+        if wnc(player_current[-1]):
+            return await ctx.send('You already won, no need to try again.', ephemeral=True)
+    
+    if len(player_current) >= 6:
+        return await ctx.send(f'Max. number of tries reached. The word was ||{wordle}||', ephemeral=True)
+    
+    
+    
+"""
