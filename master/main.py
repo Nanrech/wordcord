b220b919642bf548b5ad5c981b71bc8e3fa7cb24
Nan R <nanbusiness13@gmail.com>
2022-03-23 01:22:29 +0100

    Update 23/III/2022

--- master/main.py
+++ master/main.py
@@ -1,9 +1,5 @@
-import random
+import random, os, json
 import interactions
-import asyncio
-from interactions import Client, Message
-from interactions.ext.wait_for import wait_for, setup
-import asyncio
 
 """import sqlite3
 
@@ -20,7 +16,6 @@ SCOPES = '749015533310967828'  # Temp
 with open('../resources/token') as f:
     TOKEN = f.read()
 bot = interactions.Client(token=TOKEN)
-setup(bot, add_method=True)
 with open('../resources/types/wordles.txt') as kk:  # Temp
     w = kk.read()
     WORDS = w.split("\n")
@@ -65,7 +60,6 @@ def profiler(us):
 '''
 {
   "2": {
-    "mode": "Wordle",
     "tries": [],
     "guesses": [],
     "streak": 5,
@@ -101,93 +95,25 @@ async def on_ready():
     print("Online!")
 
 
-@bot.command(name='help', description='release me from this hell', scope=SCOPES, options=[interactions.Option(
-    name='user', description='User you want to dump', type=interactions.OptionType.STRING, required=True)])
-async def dump(ctx: interactions.CommandContext, user):
-    await ctx.send(f'... {user}')
-
-
-@bot.command(name='test', description='Sends a test command', scope=SCOPES)
+@bot.command(name='dbug', description='Sends a test command', scope=SCOPES)
 async def emb(ctx: interactions.CommandContext):
+    await ctx.send(str(ctx._json))
     await ctx.send(embeds=interactions.Embed(title="**Wordle!**",
                                              description=f"Today's current word is ||`{wordle}`||!",
                                              color=0x56AB91))
 
 
-@bot.command(name='start', description='Creates a user profile so your progress and stats can be saved. It is '
-                                       'required to play the game!', scope=SCOPES)
-async def start(ctx: interactions.CommandContext):
-    return await ctx.send(embeds=interactions.Embed(title='*Wordcord!*',
-                                                    description=f'This currently does nothing but I like embeds @Nan <@{ctx.author.user.id}>',
-                                                    
-                                                    color=0x56AB91))
-
-
-@bot.command(name='submit', description='Submit a wordle guess', scope=SCOPES, options=[interactions.Option(
+@bot.command(name='guess', description='Submit a wordle guess', scope=SCOPES, options=[interactions.Option(
     name='guess', description='A string containing your guess', type=interactions.OptionType.STRING, required=True)])
 async def submit(ctx: interactions.CommandContext, guess):
-    async with open('../resources/users.json', 'r+') as ñ:
-        print(ñ[""])
+    if not err(inp=guess, chars=valid_chr, valids=WORDS):
+        return await ctx.send("Guess was incorrectly formatted - PLACEHOLDER TEXT")
+    prf_exists(str(ctx.author.user.id))
+    with open(f"./users/{ctx.author.user.id}.json", "r") as l:
+        profile = json.load(l)
+    profile["tries"] = profile["tries"].append(gss(guess))
+    profile["guesses"] = profile["guesses"].append(guess)
 
-
-buttons = [
-    interactions.Button(style=interactions.ButtonStyle.SECONDARY, custom_id="left", label="◀"),
-    interactions.Button(style=interactions.ButtonStyle.DANGER, custom_id="stop", label="🛑"),
-    interactions.Button(style=interactions.ButtonStyle.SECONDARY, custom_id="right", label="▶")
-]
-action_row = interactions.ActionRow(components=buttons)
-mario = interactions.Button(style=interactions.ButtonStyle.DANGER, custom_id="mar", label="pls")
-embedl = [interactions.Embed(title='*1! Haha!',
-                             description='This is the 1st page.. woo',
-                             color=0x75CB18),
-          interactions.Embed(title='***2***',
-                             description='Second page, almost there!',
-                             color=0x65AB19),
-          interactions.Embed(title='The final page!',
-                             description='I wonder what the description for this one will look like',
-                             color=0x75CB18)]
-
-
-@bot.command(
-    name="awe",
-    description="this is just a test command.",
-    scope=SCOPES
-)
-async def ayep(ctx):
-    await ctx.send(embeds=embedl[0], components=action_row)
-
-
-@bot.component('left')
-async def left_bpress(ctx: interactions.ComponentContext):
-    await ctx.edit(embeds=embedl[1])
-
-
-@bot.component('right')
-async def right_bpress(ctx: interactions.ComponentContext):
-    await ctx.edit(embeds=embedl[1])
-
-
-@bot.command(
-    name="ayale", description="this is just a test command.", scope=SCOPES
-)
-async def aya(ctx: interactions.CommandContext):
-    await ctx.send("grabbing a message...")
-
-    async def check(msg):
-        if int(msg.author.id) == int(ctx.author.user.id):
-            return True
-        await ctx.send("I wasn't asking you")
-        return False
-
-    try:
-        msg: Message = await wait_for(
-            bot, "on_message_create", check=check, timeout=15
-        )
-    except asyncio.TimeoutError:
-        return await ctx.send("You said nothing :(")
-
-
-bot.start()
 
 """
 
@@ -202,7 +128,5 @@ WNC func -> returns true if all the squares in a tryfied are green
     
     if len(player_current) >= 6:
         return await ctx.send(f'Max. number of tries reached. The word was ||{wordle}||', ephemeral=True)
-    
-    
-    
+
 """
--- master/tests.py
+++ master/tests.py
@@ -1,30 +1,81 @@
-ass = {"c": 1,
-       "loo": {
-           "woo": 8,
-           "kill": {
-               "meer": 1,
-               "kat": 3
-           }
-       }}
-# print(len(ass), len(ass["loo"]), ass["loo"]["kill"])
+import json, os
 
-import sqlite3
-
-# DB stuff!
-conn = sqlite3.connect('../resources/userbase.db')
-c = conn.cursor()
+wordle = 'craft'
 
 
-def _create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS stuffToPlot(unix REAL, datestamp TEXT, keyword TEXT, value REAL)')
+def gss(guess):
+    n = 0
+    load = []
+    for x in guess.lower():
+        if x == wordle[n]:
+            load.append('🟩')
+            n += 1
+            continue
+        elif x in wordle and x != wordle[n]:
+            load.append('🟨')
+            n += 1
+            continue
+        else:
+            load.append('⬛')
+            n += 1
+            continue
+    return ''.join(load)
 
 
-def _data_entry():
-    c.execute("INSERT INTO stuffToPlot VALUES(86754865, '2016-01-01', 'Python', 8)")
-    conn.commit()
-    c.close()
-    conn.close()
+def prf_exists(user: str):
+    try:
+        with open(f"./users/{user}.json", "x") as yu:
+            with open(f"./users/{user}.json", "r") as uy:
+                try:
+                    yy = json.load(uy)
+                except:
+                    yy: dict = {}
+            yy["tries"] = []
+            yy["guesses"] = []
+            yy["streak"] = 0
+            yy["beaten"] = False
+            with open(f"./users/{user}.json", "w") as uu:
+                print(json.load(uu), yy)
+                json.dump(yy, uu)
+
+    except FileExistsError:
+        return
 
 
-_create_table()
-_data_entry()
+def clear_prf(pid: str):
+    with open(f"./users/{pid}.json", "r") as g:
+        yy = json.load(g)
+    yy["guesses"] = ""
+    yy["tries"] = ""
+    with open(f"./users/{pid}.json", "w") as g:
+        json.dump(yy, g)
+    return
+
+
+def post_tries(pid: str, tri: str):
+    with open(f"./users/{pid}.json", "r") as g:
+        d = json.load(g)
+    with open(f"./users/{pid}.json", "w") as g:
+        d["tries"] = d["tries"] + tri
+        json.dump(d, g)
+    return
+
+
+def post_guess(pid: str, gus: str):
+    with open(f"./users/{pid}.json", "r") as g:
+        d = json.load(g)
+    with open(f"./users/{pid}.json", "w") as g:
+        d["guesses"] = d["guesses"] + gus
+        json.dump(d, g)
+    return
+
+
+def get_tries(pid: str):
+    with open(f"./users/{pid}.json", "r") as g:
+        return str(dict(json.load(g))["tries"])
+
+
+guess = "craft"
+#clear_prf('76576576576')
+post_tries("76576576576", gss(guess))
+post_guess("76576576576", guess)
