4ba059581d904610e615cb5ba40e817304524908
Nan R <nanbusiness13@gmail.com>
2022-03-29 00:51:11 +0200

    29/03/22 2

--- master/main.py
+++ master/main.py
@@ -53,7 +53,7 @@ async def _emb(ctx: interactions.CommandContext):
                                              color=0x56AB91))
 
 
-@bot.command(name="admin-clear", description="Removes a player\"s profile", scope=SCOPES, options=[
+@bot.command(name="admin-clear", description="Removes a player's profile", scope=SCOPES, options=[
     interactions.Option(name="user", description="User", type=interactions.OptionType.USER, required=True)])
 async def _soft_clear(ctx: interactions.CommandContext, user: interactions.Member):
     try:
@@ -89,11 +89,45 @@ async def submit(ctx: interactions.CommandContext, guess):
     msg = interactions.Embed(
         title=f"{emji} Wordle",
         color=clr,
-        fields=[interactions.EmbedField(name="Attempts:", value=fields)],
-        ephemeral=True)
+        fields=[interactions.EmbedField(name="Attempts:", value=fields)])
     # (name="Tries:\n", value="\n".join([tries[x:x+5] for x in range(0, len(tries), 5)]))]) for JUST the squares
 
-    await ctx.send(embeds=msg)
+    await ctx.send(embeds=msg, ephemeral=True)
+
+
+@bot.command(name="current", description="Shows your current game's status. Can be public or private",
+             scope=SCOPES,
+             options=[interactions.Option(name="show",
+                                          description="Whether to show this to the public or not",
+                                          required=False,
+                                          type=interactions.OptionType.STRING,
+                                          choices=[
+                                              interactions.Choice(name="Show", value="ye"),
+                                              interactions.Choice(name="Don't show", value="na")])])
+async def _status(ctx: interactions.CommandContext, show: str = None):
+    U = ctx.author.id
+    tries = str(dict(fetch_profile(U))["tries"])
+    if tries == "":
+        return await ctx.send(content="No current session found", ephemeral=True)
+    guesses = str(dict(fetch_profile(U))["guesses"]).upper()
+    U = ctx.author.user.id
+    a = [tries[x:x + 5] for x in range(0, len(tries), 5)]
+    b = [guesses[x:x + 5] for x in range(0, len(guesses), 5)]
+    fields = "\n".join(' '.join(x) for x in zip(a, b))
+    if wnc(dict(fetch_profile(U))["tries"][-5:]):
+        emji = "<:A_s:958125488642088980> "
+        clr = 0x6aaa64
+    else:
+        emji = "<:A_p:958127218142347356> "
+        clr = 0xffde59
+    msg = interactions.Embed(
+        title=f"{emji} Wordle",
+        color=clr,
+        fields=[interactions.EmbedField(name="Attempts:", value=fields)])
+    if show == "ye":
+        await ctx.send(embeds=msg, ephemeral=False)
+    else:
+        await ctx.send(embeds=msg, ephemeral=True)
 
 
 bot.start()
